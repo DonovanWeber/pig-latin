c4209fa963932f2f9a50420bca9260269145f1ec
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-08 14:15:20 -0700

    add functioality to our function to alway multiple words to be translated
    Co-authored-by: DonovanWeber <donovanweber03@gmail.com>

--- README.md
+++ README.md
@@ -7,21 +7,27 @@ Expected Output: "away"
 
 Test: "It will add 'way' to the end of words that only begin with a vowel"
 Code:
-let newArray = [];
-let vowels = ["a", "e", "i", "o", "u"]
+let newArray [];
+let vowels ["a", "e", "i", "o", "u"]
 pigLatin("tkay");
 Expected Output: ""
 
 Test: "It will return a pig latin string if there is no vowel at the beginning."
 Code: 
-let newArray[];
-let vowels["a", "e", "i", "o", "u"];
+let newArray [];
+let vowels ["a", "e", "i", "o", "u"];
 pigLatin('vakay');
 Expected Output:"vakay"
 
 Test: "It will add "way" to the end of words that begin with a vowel and are uppercased."
 Code:
-let newArray[];
-let vowels["a", "e", "i", "o", "u"];
+let newArray [];
+let vowels ["a", "e", "i", "o", "u"];
 pigLatin("A");
-expected Output: "away"
+expected Output: "Away"
+
+Test: "It will find the beginning letters in two words and add "way" if the word begins with a vowel"
+Code: 
+let newArray[];
+let vowels ["a", "e", "i", "o", "u"];
+pigLa
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
+
 function pigLatin(text) {
   let newArray = [];
   let vowels = ["a", "e", "i", "o", "u"]
   console.log(vowels);
-  const textArray = text.split("");
-    if (vowels.includes(text.toLowerCase()[0])) {
-      newArray.push(textArray + "way");
+  const textArray = text.split(" ");
+  console.log(textArray);
+  for (let i = 0; i < textArray.length; i++) {
+    let letterArray = textArray[i].split("");
+    console.log(letterArray);
+    if (vowels.includes(letterArray[0].toLowerCase())) {
+      newArray.push(textArray[i] + "way ");
+    } else if ((vowels.includes(letterArray[0].toLowerCase())) && (vowels.includes(letterArray[1].toLowerCase()))) {
+      newArray.push(textArray[i] + "way ")
     } else {
-        newArray.push(textArray);      
+        newArray.push(textArray[i]+ " ");      
     }
-  newArray.join();
+  }
+  newArray.join(" ");
   return newArray.toString().replace(/,/g, '');
 }
-pigLatin("vakay");
+pigLatin("akay okay");
 
 
 // function pigLatin(text) {
@@ -19,14 +27,31 @@ pigLatin("vakay");
 //   let vowels = ["a", "e", "i", "o", "u"]
 //   console.log(vowels);
 //   const textArray = text.split("");
-//   for (i = 0; i <= 0; i++) {
-//     if (vowels.includes(textArray[i])) {
+//   if (vowels.includes(text.toLowerCase()[0])) {
 //       newArray.push(textArray + "way");
+//     } else if {
+      
 //     } else {
 //         newArray.push(textArray);      
 //     }
-//   }
 //   newArray.join();
 //   return newArray.toString().replace(/,/g, '');
 // }
-// pigLatin("vakay");
+// pigLatin("akay okay");
+
+// function pigLatin(text) {
+//   let newArray = [];
+//   let vowels = ["a", "e", "i", "o", "u"]
+//   console.log(vowels);
+//   const textArray = text.split("");
+//     if (vowels.includes(text.toLowerCase()[0])) {
+//       newArray.push(textArray + "way");
+//     } else if ((vowels.includes(text.toLowerCase()[0])) && (vowels.includes(text.toLowerCase()[1]))) {
+//       newArray.push(textArray[1] + "way")
+//     } else {
+//         newArray.push(textArray);      
+//     }
+//   newArray.join();
+//   return newArray.toString().replace(/,/g, '');
+// }
+// pigLatin("akay okay");
